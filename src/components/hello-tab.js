1835ff9bc1f16a62661c51af11b21f20ac7ed9d7
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-15 16:42:43 -0700

    linting hello-tab

--- src/components/hello-tab.js
+++ src/components/hello-tab.js
@@ -3,33 +3,33 @@ export default class helloTab {
     this.helloInfo();
   }
   helloInfo() {
-    let page = document.getElementById('main');
-    let tab = document.createElement('div');
+    const page = document.getElementById('main');
+    const tab = document.createElement('div');
     page.append(tab);
     tab.classList = 'container';
     tab.id = 'hello-tab';
-    let divHello = document.createElement('div');
+    const divHello = document.createElement('div');
     divHello.id = 'text-hello-tab';
     divHello.classList = 'column is-three-fifths';
-    let helloColumns = document.createElement('div');
+    const helloColumns = document.createElement('div');
     helloColumns.classList = 'columns';
     tab.appendChild(helloColumns);
     helloColumns.appendChild(divHello);
 
-    let helloTitle = document.createElement('h1');
+    const helloTitle = document.createElement('h1');
     helloTitle.classList = 'title is-3 is-spaced green-text has-text-centered';
     helloTitle.innerHTML = 'Welcome to the best coffee shop ever';
     divHello.appendChild(helloTitle);
 
-    let helloSubt = document.createElement('h2');
+    const helloSubt = document.createElement('h2');
     helloSubt.classList = 'subtitle is-5 green-text has-text-centered';
-    helloSubt.innerHTML =
-      'Relax your paws, have some foam while your human has some greens';
+    helloSubt.innerHTML = 'Relax your paws! ';
+    helloSubt.innerHTML += 'Have some foam while your human has some greens';
     divHello.appendChild(helloSubt);
 
-    let helloPup = document.createElement('div');
+    const helloPup = document.createElement('div');
     helloPup.classList = 'column image img-column';
-    let pup = document.createElement('img');
+    const pup = document.createElement('img');
     pup.classList = 'is-rounded pup';
     pup.src = 'https://i.imgur.com/9uKrhtz.png';
     helloPup.appendChild(pup);
@@ -38,6 +38,6 @@ export default class helloTab {
 }
 
 export const displayWelcome = () => {
-  let welcome = new helloTab();
+  const welcome = new helloTab();
   return welcome;
 };
