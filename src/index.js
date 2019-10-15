a70bec41ab28bf2135da9ca85ffd062ee64fc6aa
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-14 20:50:21 -0700

    updating index js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 require('./../dist/css/mystyles.scss');
 import Banner from './components/home-banner';
 import infoContainer from './components/home-container';
+import tabs from './components/tabs';
+import helloTab from './components/hello-tab';
+import displayWelcome from './components/hello-tab';
+import { displayAll } from './components/menu-tab';
+import { displayFind } from './components/find-tab';
+import findTab from './components/find-tab';
+import tabsContainer from './components/tabs';
 
 const displayHome = () => {
   let display = document.getElementById('main');
-  console.log(display);
   let header = new Banner();
-  console.log(header + 'header return');
-  let tabBox = new infoContainer();
-  console.log(tabBox + 'tabbox return');
-  // display.appendChild(header);
-  // display.appendChild(tabBox);
+  // let tabBox = new infoContainer();
+  let tabBox = new tabsContainer();
+  tabBox.tabs();
+  return display, header, tabBox;
 };
 
+// const displayWelcome = () => {
+//   let welcome = new helloTab();
+//   welcome;
+// };
+
+// const displayAll = () => {
+//   displayCoffeeMenu();
+//   displayPastryMenu();
+//   displaySaladMenu();
+// };
+
+// const displayFind = () => {
+//   let find = new findTab();
+//   find;
+// };
+
 displayHome();
+// displayWelcome();
+// displayAll();
+// displayFind();
