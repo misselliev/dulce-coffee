0416462712ec44482a8b45c798662eec0f03d86e
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-15 15:53:08 -0700

    lint fixes for find tab

--- src/components/find-tab.js
+++ src/components/find-tab.js
@@ -2,24 +2,24 @@ export default class findTab {
   constructor() {
     this.findInfo();
   }
+
   findInfo() {
-    let page = document.getElementById('main');
-    let tab = document.createElement('div');
+    const page = document.getElementById('main');
+    const tab = document.createElement('div');
     page.append(tab);
     tab.classList = 'container';
     tab.id = 'find-us-tab';
-    let imageCont = document.createElement('div');
+    const imageCont = document.createElement('div');
     imageCont.classList = 'flex-container';
-    let picture = document.createElement('img');
+    const picture = document.createElement('img');
     picture.classList = 'image is-squared extra-space';
     picture.src =
       'https://images.unsplash.com/photo-1469631423273-6995642a6a40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=778&q=80';
     imageCont.appendChild(picture);
     tab.appendChild(imageCont);
-    let divInfo = document.createElement('div');
-    let info = document.createElement('h1');
-    info.classList =
-      'title is-3 is-spaced italic-text green-text extra-space has-text-centered';
+    const divInfo = document.createElement('div');
+    const info = document.createElement('h1');
+    info.classList = 'title is-3 italic-text green-text has-text-centered';
     info.innerHTML = 'Soon in a place near you!';
     divInfo.appendChild(info);
     tab.appendChild(divInfo);
@@ -27,6 +27,6 @@ export default class findTab {
 }
 
 export const displayFind = () => {
-  let find = new findTab();
-  find;
+  const Find = new findTab();
+  return Find;
 };
