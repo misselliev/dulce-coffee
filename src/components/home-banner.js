8f332dbce725868f54033744004804957dd14419
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-15 16:52:54 -0700

    linting fixes for home-banner

--- src/components/home-banner.js
+++ src/components/home-banner.js
@@ -2,27 +2,27 @@ export default class Banner {
   constructor() {
     this.homeBanner();
   }
+
   homeBanner() {
-    let bannerMainContainer = document.createElement('section');
+    const bannerMainContainer = document.createElement('section');
     bannerMainContainer.classList = 'hero is-primary is-bold header-bg';
-    let bannerBody = document.createElement('div');
+    const bannerBody = document.createElement('div');
     bannerBody.classList = 'hero-body';
     bannerMainContainer.appendChild(bannerBody);
-    let bannerCont = document.createElement('div');
+    const bannerCont = document.createElement('div');
     bannerBody.appendChild(bannerCont);
 
-    let title = document.createElement('h1');
+    const title = document.createElement('h1');
     title.classList = 'title is-spaced header-text';
     title.innerHTML = 'The Green Woof';
     bannerCont.appendChild(title);
 
-    let subtitle = document.createElement('h2');
-    subtitle.innerHTML =
-      'When your human wants to be vegan but you want a puppachino';
+    const subtitle = document.createElement('h2');
+    subtitle.innerHTML = 'When your human is vegan but you want a puppachino';
     subtitle.classList = 'subtitle header-text';
     bannerCont.appendChild(subtitle);
 
-    let display = document.getElementById('main');
+    const display = document.getElementById('main');
     display.appendChild(bannerMainContainer);
   }
 }
